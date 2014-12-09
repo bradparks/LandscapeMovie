9b98c53e8b05599d8a3b77466679b1ea011246af
Mo DeJong <modejong@Mos-MacBook-Air.local>
2014-12-09 12:35:48 -0800

    Initial Commit

---
+++ LandscapeMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LandscapeMovie
+//
+//  Created by Mo DeJong on 12/9/14.
+//  Copyright (c) 2014 helpurock software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LandscapeMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LandscapeMovie
+//
+//  Created by Mo DeJong on 12/9/14.
+//  Copyright (c) 2014 helpurock software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
